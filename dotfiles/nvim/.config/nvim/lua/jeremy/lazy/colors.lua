4d8779264365fc87b15e283edb7ea8536832fd09
jeremy <mute.coqui@gmail.com>
2024-07-29 07:25:49 -0500

    Adding some nvim plugins and picom and other settings

--- dotfiles/nvim/.config/nvim/lua/jeremy/lazy/colors.lua
+++ dotfiles/nvim/.config/nvim/lua/jeremy/lazy/colors.lua
@@ -2,15 +2,14 @@
 function ColorMyPencils(color)
 	color = color or "kanagawa-wave"
 	vim.cmd.colorscheme(color)
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
 	"rebelot/kanagawa.nvim",
 	config = function()
 		require("kanagawa").setup({
-			-- disable_background = true,
 			ColorMyPencils()
 		})
 	end
